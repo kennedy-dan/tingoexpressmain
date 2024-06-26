d97849ab23810e78466552d5c8d604567ee3497e
kennedy-dan <kenidankid@yahoo.com>
2024-06-26 20:09:15 +0100

    index chnage

---
+++ pages/location/index.jsx
@@ -0,0 +1,13 @@
+import HomeLayout from '@/components/Layout/HomeLayout'
+import ShopLocation from '@/components/LocationModule/ShopLocation'
+import React from 'react'
+
+const Location = () => {
+  return (
+    <HomeLayout>
+        <ShopLocation />
+    </HomeLayout>
+  )
+}
+
+export default Location
