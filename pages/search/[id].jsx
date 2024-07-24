1fc1b1c0b9613632d419927887e83eea5a409a62
kennedy-dan <kenidankid@yahoo.com>
2024-07-24 12:46:53 +0100

    errr

--- pages/search/[id].jsx
+++ pages/search/[id].jsx
@@ -3,15 +3,17 @@ import SearchResult from '@/components/SearchModule/SearchResult'
 import { getAllProducts } from '@/store/slice/productSlice'
 import { useRouter } from 'next/router'
 import React,{useEffect} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Search = () => {
   const dispatch = useDispatch()
   const router = useRouter()
+  const {token} = useSelector(state => state.auth)
+
   const {id} = router.query
   console.log(id)
   useEffect(() => {
-    if(id) {
+    if(id && token) {
       const data = {
         name: id
       }
