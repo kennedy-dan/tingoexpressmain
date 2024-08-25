d911e9b289ff203d1cce8d7ebc7a21b38121e22d
kennedy-dan <kenidankid@yahoo.com>
2024-08-25 15:55:56 +0100

    contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="bg-[#E7EBF6] py-20 px-10 lg:px-[20px] lg:py-[20px] xl:px-[100px] xl:py-[100px] md:flex items-center justify-between ">
           <div>
             <p>{"Product category" < "frozenfoods"} </p>
-            <p className="text-[54px] font-bold text-black font-montserrat">
+            <p className="md:text-[54px] text-[22px] font-bold text-black font-montserrat">
               Contact Us
             </p>
           </div>
@@ -17,20 +17,20 @@ const Contact = () => {
           </div>
         </div>
 
-        <div className="md:flex py-20 px-10 lg:px-[20px] lg:py-[20px] xl:px-[100px] xl:py-[100px] font-montserrat">
+        <div className="md:flex py-20 px-6 lg:px-[20px] lg:py-[20px] xl:px-[100px] xl:py-[100px] font-montserrat">
           <div className="md:w-1/2 font-montserrat ">
-            <p className="font-semibold text-[34px] ">Leave A Message</p>
-            <div className="mt-16">
-              <p className="font-semibold">Full Name</p>
+            <p className="font-semibold text-[18px] md:text-[34px] ">Leave A Message</p>
+            <div className="md:mt-16 mt-10">
+              <p className="font-semibold md:text-base text-[16px]">Enter Full Name</p>
               <input
                 placeholder="Enter Your Email Address"
-                className=" w-[96%] pl-5 outline-none border border-[#C9C5CC] bg-[#F9F9F9] rounded-lg py-5"
+                className=" w-full md:w-[96%] pl-5 outline-none border border-[#C9C5CC] bg-[#F9F9F9] rounded-lg py-5"
               />
             </div>{" "}
             <div className="mt-4">
-              <p className="font-semibold">Password</p>
+              <p className="font-semibold md:text-base text-[16px]">Email</p>
               <input
-                placeholder="Create a unique password"
+                placeholder="Enter Email"
                 className=" w-[96%] pl-5 outline-none border border-[#C9C5CC] bg-[#F9F9F9] rounded-lg py-5"
               />
             </div>
@@ -38,7 +38,7 @@ const Contact = () => {
               <p className="text-[#4FA9AB] text-[16px]">Forget password</p>
             </div> */}
             <div className="mt-4">
-              <p className="font-semibold">Message</p>
+              <p className="font-semibold md:text-base text-[16px]">Message</p>
               <textarea
                 placeholder="Type Your Message Here"
                 className=" w-[96%] pl-5 outline-none border border-[#C9C5CC] bg-[#F9F9F9] rounded-lg py-5"
@@ -51,24 +51,24 @@ const Contact = () => {
             </div>
           </div>
           <div className="md:w-1/2 md:mt-0 mt-20 ">
-            <p className="font-semibold text-[34px] ">
+            <p className="font-semibold text-[18px] md:text-[34px] ">
               We would love to hear from you!
             </p>
-            <p className="text-[20px] mt-16 font-semibold " >HEAD OFFICE</p>
-            <p className="font-[400] text-[16px] pt-3 pb-2 " >
+            <p className="md:text-[20px] text-[16px] mt-8 md:mt-16 font-semibold " >HEAD OFFICE</p>
+            <p className="font-[400] md:text-[16px] text-[13px] pt-3 pb-2 " >
               Lorem ipsum dolor sit amet consectetur. Sodales velit elementum
               gravida nibh ultrices urna egestas lectus tempus. Eu eu dui tellus
               mattis feugiat et elementum convallis sit. Velit porttitor nam
               auctor a commodo at risus nisl elementum.
             </p>
-            <p className="text-[20px] mt-1 font-semibold " >BUSINESS HOURS</p>
-            <p className="font-[400] text-[16px] pt-3 pb-5 " >
+            <p className="md:text-[20px] text-[16px]] mt-1 font-semibold " >BUSINESS HOURS</p>
+            <p className="font-[400] md:text-[16px] text-[13px] pt-3 pb-5 " >
               Lorem ipsum dolor sit amet consectetur. Sodales velit elementum
               gravida nibh ultrices urna egestas lectus tempus. Eu eu dui tellus
               mattis feugiat et elementum convallis sit. Velit porttitor nam
               auctor a commodo at risus nisl elementum.
-            </p>    <p className="text-[20px] mt-1 font-semibold " >CONTACT INFORMATION</p>
-            <p className="font-[400] text-[16px] pt-3 pb-5 " >
+            </p>    <p className="md:text-[20px] text-[16px] mt-1 font-semibold " >CONTACT INFORMATION</p>
+            <p className="font-[400] md:text-[16px] text-[13px] pt-3 pb-5 " >
               Lorem ipsum dolor sit amet consectetur. Sodales velit elementum
               gravida nibh ultrices urna egestas lectus tempus. Eu eu dui tellus
               mattis feugiat et elementum convallis sit. Velit porttitor nam
